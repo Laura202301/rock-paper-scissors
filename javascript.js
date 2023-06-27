fb4ea9dd7d69e7d44c6e7876e6f5e9760dc2a8c2
Laura202301 <fedele.laura17@gmail.com>
2023-06-27 17:38:45 -0300

    Edit javascript file to add DOM methods

--- javascript.js
+++ javascript.js
@@ -1,37 +1,92 @@
-
 function getComputerChoice(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-let computerSelection = getComputerChoice(1, 3);
-
-if (computerSelection === 1) {
-    computerSelection = "rock";
-} else if (computerSelection === 2) {
-    computerSelection = "paper";
-} else {
-    computerSelection = "scissors";
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-console.log(computerSelection)
+let playerScore = 0;
+let computerScore = 0;
+let roundsPlayed = 0;
 
-//let playerSelection = String(prompt("write rock, paper or scissors").toLowerCase());
+function playRound(playerSelection) {
+  let computerSelection = getComputerChoice(1, 3);
 
-function playRound(playerSelection, computerSelection) {
-let result = 0;
-    if (computerSelection === "rock" && playerSelection === "paper" ||computerSelection === "scissors" && playerSelection === "rock" ||computerSelection === "paper" && playerSelection === "scissors") {
-        return ++result + " " + "You win!";
+  if (computerSelection === 1) {
+    computerSelection = "ROCK";
+  } else if (computerSelection === 2) {
+    computerSelection = "PAPER";
+  } else {
+    computerSelection = "SCISSORS";
+  }
 
-    } else if (computerSelection === "paper" && playerSelection === "rock" ||
-    computerSelection === "rock" && playerSelection === "scissors" ||
-    computerSelection === "scissors" && playerSelection === "paper") {
-        return ++result + " " + "You Lose!";
+  let playerChoiceElement = document.querySelector(".player-choice");
+  playerChoiceElement.textContent = playerSelection;
+
+  let computerChoiceElement = document.querySelector(".computer-choice");
+  computerChoiceElement.textContent = computerSelection;
+
+  if (
+    (computerSelection === "ROCK" && playerSelection === "PAPER") ||
+    (computerSelection === "SCISSORS" && playerSelection === "ROCK") ||
+    (computerSelection === "PAPER" && playerSelection === "SCISSORS")
+  ) {
+    playerScore++;
+  } else if (
+    (computerSelection === "PAPER" && playerSelection === "ROCK") ||
+    (computerSelection === "ROCK" && playerSelection === "SCISSORS") ||
+    (computerSelection === "SCISSORS" && playerSelection === "PAPER")
+  ) {
+    computerScore++;
+  }
+
+  let playerScoreElement = document.querySelector('.player-score');
+  playerScoreElement.textContent = playerScore;
+
+  let computerScoreElement = document.querySelector('.computer-score');
+  computerScoreElement.textContent = computerScore;
+
+  if (playerScore !== computerScore) {
+    roundsPlayed++;
+  }
+
+  if (roundsPlayed === 5) {
+    let playTextElement = document.querySelector('.play-text');
+    let imageElement = document.querySelector('.img-challenge');
     
-    } else (computerSelection === playerSelection); { 
-        return result + " " + "it's a tie"; 
+    if (playerScore > computerScore) {
+      playTextElement.textContent = "PLAYER WINS!";
+      imageElement.src = "./images/celebration.jpg";
+      imageElement.alt = "Picture of celebration confetti";
+    } else if (playerScore < computerScore) {
+      playTextElement.textContent = "COMPUTER WINS!";
+      imageElement.src = "./images/celebration.jpg";
+      imageElement.alt = "Picture of celebration confetti";
+    } else {
+      playTextElement.textContent = "IT'S A TIE!";
     }
+  }
 }
 
-//const playerSelection = "paper";
-console.log(playerSelection)
-console.log(playRound(playerSelection, computerSelection));
+let rockButton = document.querySelector(".rock-button");
+let paperButton = document.querySelector(".paper-button");
+let scissorsButton = document.querySelector(".scissors-button");
 
+rockButton.addEventListener("click", function() {
+  if (roundsPlayed < 5) {
+    playRound("ROCK");
+  }
+});
+
+paperButton.addEventListener("click", function() {
+  if (roundsPlayed < 5) {
+    playRound("PAPER");
+  }
+});
+
+scissorsButton.addEventListener("click", function() {
+  if (roundsPlayed < 5) {
+    playRound("SCISSORS");
+  }
+});
+
+document.querySelector(".play-again").addEventListener("click", function() {
+  location.reload();
+});
